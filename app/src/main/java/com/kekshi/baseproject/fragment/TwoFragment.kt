9c7bf452ed1ab12518c7335920db9f114e1ff909
理想国 <364112371@qq.com>
2019-07-31 15:59:29 +0800

    加入fragment懒加载

--- app/src/main/java/com/kekshi/baseproject/fragment/TwoFragment.kt
+++ app/src/main/java/com/kekshi/baseproject/fragment/TwoFragment.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.kekshi.baselib.base.BaseFragement
 import com.kekshi.baseproject.R
 
-class TwoFragment : Fragment() {
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
+class TwoFragment : BaseFragement() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.fragment_two_layout, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun lazyLoad() {
+
     }
 }
---
+++ baselib/src/main/java/com/kekshi/baselib/base/BaseFragement.kt
@@ -0,0 +1,49 @@
+package com.kekshi.baselib.base
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+abstract class BaseFragement : Fragment() {
+
+    /**
+     * View有没有加载过
+     */
+    protected var isViewInitiated: Boolean = false
+    /**
+     * 页面是否可见
+     */
+    protected var isVisibleToUser: Boolean = false
+    /**
+     * 是不是加载过
+     */
+    protected var isDataInitiated: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        isViewInitiated = true
+        loadData()
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        this.isVisibleToUser = isVisibleToUser
+        loadData()
+    }
+
+    /**
+     *懒加载，Fragment可见的时候调用这个方法，而且只调用一次
+     */
+    abstract fun lazyLoad()
+
+    private fun loadData() {
+        if (isVisibleToUser && isViewInitiated && !isDataInitiated) {
+            isDataInitiated = true
+            lazyLoad()
+        }
+    }
+}
